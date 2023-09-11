4acd839312ab06dbc1d1e1ed2d7bef1cad8e1933
MasterRomantic <70520086+MasterRomantic@users.noreply.github.com>
2023-09-11 16:00:45 +0200

    Step 3: Create Property Binding
    The text on the screen is now derived from model data.

--- webapp/index.js
+++ webapp/index.js
@@ -1,14 +1,23 @@
 sap.ui.require([
-    "sap/m/Text"
-], function (Text) {
+    "sap/m/Text",
+    "sap/ui/model/json/JSONModel"
+], function (Text, JSONModel) {
     "use strict";
 
     // Attach an anonymous function to the SAPUI5 'init' event
     // NB: dont use "sap.ui.getCore()" in production apps, rather "this.getView().getModel()"
     sap.ui.getCore().attachInit(function() {
-        // Create a text UI element that displays a hardcoded text string
+        //Create a JSON model from an object literal
+        var oModel = new JSONModel({
+            greetingText: "Hi. my name is Batman"
+        });
+
+        // Assign the model object to the SAPUI5 core (NB on getCore)
+        sap.ui.getCore().setModel(oModel);
+
+        // Display a text element whose text is derived from the model object
         new Text({
-            text: "Hi. my name is Batman"
+            text: "{/greetingText}"
         }).placeAt("content");
     });
 })
