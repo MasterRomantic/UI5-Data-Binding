065f683f693f0e004a0e67a46f6ce0f4ba4704e2
MasterRomantic <70520086+MasterRomantic@users.noreply.github.com>
2023-09-11 16:31:23 +0200

    Step 4: Two-Way Data Binding
    We now change the user interface so that the first and last name fields are displayed using sap.m.Input fields and an additional check box control is used to enable or disable both input fields. This arrangement illustrates a feature known as "two-way data binding". Now that the view contains more controls, we will also move the view definition into an XML file.

--- webapp/index.js
+++ webapp/index.js
@@ -1,7 +1,7 @@
 sap.ui.require([
-    "sap/m/Text",
-    "sap/ui/model/json/JSONModel"
-], function (Text, JSONModel) {
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/core/mvc/XMLView"
+], function (JSONModel, XMLView) {
     "use strict";
 
     // Attach an anonymous function to the SAPUI5 'init' event
@@ -9,15 +9,18 @@ sap.ui.require([
     sap.ui.getCore().attachInit(function() {
         //Create a JSON model from an object literal
         var oModel = new JSONModel({
-            greetingText: "Hi. my name is Batman"
+            firstName: "Bruce",
+            lastName: "Wayne",
+            enabled: true,
+            panelHeaderText: "Data Binding Basics"
         });
 
         // Assign the model object to the SAPUI5 core (NB on getCore)
         sap.ui.getCore().setModel(oModel);
 
-        // Display a text element whose text is derived from the model object
-        new Text({
-            text: "{/greetingText}"
+        // Display the XML view called "App"
+        new XMLView({
+            viewName: "sap.ui.demo.db.View.App"
         }).placeAt("content");
     });
 })
