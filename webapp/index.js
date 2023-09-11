fa6ca4a73fa4748b680f649a7ebd112a577577ce
MasterRomantic <70520086+MasterRomantic@users.noreply.github.com>
2023-09-11 16:59:41 +0200

    Step 6: Resource Models
    We place all translatable texts (such as field labels) into a resource bundle.

--- webapp/index.js
+++ webapp/index.js
@@ -1,8 +1,9 @@
 sap.ui.require([
     "sap/ui/model/json/JSONModel",
     "sap/ui/core/mvc/XMLView",
-    "sap/ui/model/BindingMode"
-], function (JSONModel, XMLView, BindingMode) {
+    "sap/ui/model/BindingMode",
+    "sap/ui/model/resource/ResourceModel"
+], function (JSONModel, XMLView, BindingMode, ResourceModel) {
     "use strict";
 
     // Attach an anonymous function to the SAPUI5 'init' event
@@ -16,8 +17,24 @@ sap.ui.require([
             panelHeaderText: "Data Binding Basics"
         });
 
+        // Create a resource bundle for language specific texts
+		// the configured supportedLocales represent the i18n files present:
+		// * "" - i18n/i18n.properties
+		// the configured fallbackLocale should represent one of these files
+		// * "" - according to the fallback chain the root bundle is the last fallback.
+		//   Configuring it explicitly avoids side effects when additional resource files are added.
+		// @see https://ui5.sap.com/#/topic/ec753bc539d748f689e3ac814e129563
+        var oResourcemodel = new ResourceModel({
+            bundleName: "sap.ui.demo.db.i18n.i18n",
+            supportedLocales: ["", "de"],
+            fallbackLocale: ""
+        });
+
+        // Assign the model object to the SAPUI5 core using the name "i18n"
+        sap.ui.getCore().setModel(oResourcemodel, "i18n");
+
         // We set the data binding to one way data binding
-        oModel.setDefaultBindingMode(BindingMode.OneWay);
+        // oModel.setDefaultBindingMode(BindingMode.OneWay);
 
         // Assign the model object to the SAPUI5 core (NB on getCore)
         sap.ui.getCore().setModel(oModel);
