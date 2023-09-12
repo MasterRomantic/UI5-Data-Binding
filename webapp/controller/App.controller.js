0cb5046b6f6538a2991dfc0e515423ecad47a416
MasterRomantic <70520086+MasterRomantic@users.noreply.github.com>
2023-09-12 10:31:31 +0200

    Step 15: Aggregation Binding Using a Factory Function
    Instead of hard-coding a single template control, we use a factory function to generate different controls based on the data received at runtime. This approach is much more flexible and allows complex or heterogeneous data to be displayed.

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -3,8 +3,9 @@ sap.ui.define([
 	"sap/m/library",
     "sap/ui/core/Locale",
     "sap/ui/core/LocaleData",
-    "sap/ui/model/type/Currency"
-], function(Controller, mobileLibrary, Locale, LocaleData, Currency) {
+    "sap/ui/model/type/Currency",
+    "sap/m/ObjectAttribute"
+], function(Controller, mobileLibrary, Locale, LocaleData, Currency, ObjectAttribute) {
     'use strict';
     return Controller.extend("sap.ui.demo.db.controller.App", {
         formatMail: function(sFirstName, sLastName) {
@@ -28,6 +29,29 @@ sap.ui.define([
             var sPath = oContext.getPath();
             var oProductDetailPanel = this.byId("productDetailsPanel");
             oProductDetailPanel.bindElement({ path: sPath, model: "products"});
+        },
+        productListFactory: function (sId, oContext) {
+            var oUIControl;
+
+            // decide based on the data on which dependent to clone
+            if (oContext.getProperty("UnitsInStock") === 0 && oContext.getProperty("Discontinued")) {
+                // the item is discontinued so use a standard list item
+                oUIControl = this.byId("productSimple").clone(sId);
+            } else {
+                // The item is available so we will create an object list item
+                oUIControl = this.byId("productExtended").clone(sId);
+
+                // if the item is temporarily out of stock, so we will add a status
+                if (oContext.getProperty("UnitsInStock") < 1) {
+                    oUIControl.addAttribute(new ObjectAttribute({
+						text : {
+							path: "i18n>outOfStock"
+						}
+					}));
+                }
+            }
+
+            return oUIControl;
         }
     });
 });
